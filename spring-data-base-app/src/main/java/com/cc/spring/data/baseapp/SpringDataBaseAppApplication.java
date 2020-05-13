8da83d7e6005bf2e2cab71ff5ae485c7f95494ea
musibs <musib.somnath@gmail.com>
2020-05-13 18:42:01 +0530

    Base application structure

---
+++ spring-data-base-app/src/main/java/com/cc/spring/data/baseapp/SpringDataBaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.cc.spring.data.baseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataBaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataBaseAppApplication.class, args);
+	}
+
+}
---
+++ spring-data-base-app/src/test/java/com/cc/spring/data/baseapp/SpringDataBaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.spring.data.baseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataBaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
